7bfdc1514bc06998c1d30783de12983b28198120
Mayant Mukul <mayantmukul@outlook.in>
2022-09-16 23:09:48 +0530

    chore: make vm executable read input files

--- src/vm/main.ts
+++ src/vm/main.ts
@@ -1,15 +1,22 @@
+import fs from 'fs/promises'
 import { Program } from '../common/asm'
 import { logger } from '../common/logger'
 import { VM } from './vm'
 
-const TEST_PROGRAMS: Program[] = []
-
 async function main() {
-    for (const program of TEST_PROGRAMS) {
-        const context = new VM(program)
-        const result = context.run()
-        logger.info(`[driver] Result: `, result)
+    const inputPath = process.argv[2]
+    if (!inputPath) {
+        throw Error("No input file provided")
     }
+
+    const inputFile = await fs.readFile(inputPath, {
+        encoding: 'utf8'
+    })
+    const program: Program = JSON.parse(inputFile)
+
+    const context = new VM(program)
+    const result = context.run()
+    logger.info(`[driver] Result: `, result)
 }
 
 main()
